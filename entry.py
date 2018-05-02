df01834bd10a13067cd1c877c062f857536b6131
Alistair <alistairedwardjenkins@gmail.com>
2018-05-02 22:09:56 +0100

    Amendments
    search by time spent option added,
    previous option added to search results display

--- entry.py
+++ entry.py
@@ -5,97 +5,109 @@ import os
 
 
 class Entry():
-	
-	'''Entry class is responsible for handling individual work log entries
-	stored in the accompanying .csv file for this project, work_log.csv
-	Entry class has methods for adding an entry to this file, searching for
-	entries within the file and editing entries within the
-	file, including deletion.
-	All matches to a search are stored in dictionaries, the key of each
-	dictionary indexing a search match; the value details the work log entry
-	details of the corresponding search match. Indexing entries within the
-	file allows entries to be edited, deleted as required.
-	'''
-	
-	FILE = 'work_log.csv'
-	
-	def add_entry_to_file(self, *args):
-		# entries are added to .csv file line by line
-	
-		with open(self.FILE, 'a') as csvfile:
-			csv_writer = csv.writer(csvfile, delimiter=' ',
-									lineterminator='\n')
-			csv_writer.writerow(args)
-	
-	def search_by_date(self, date):
-		matches = {}
-		with open(self.FILE, 'r') as csvfile:
-			csv_reader = csv.reader(csvfile, delimiter=' ',
-									lineterminator='\n')
-			for line_num, line in enumerate(csv_reader):
-				if line:
-					if line[0] == date:
-						matches[line_num] = line
-	
-		return matches
-	
-	def search_by_range_of_dates(self, start_date, end_date):
-		matched = {}
-		for date in self.daterange(start_date, end_date):
-			matched.update(self.search_by_date(date.strftime('%d/%m/%Y')))
-		return matched
-	
-	def daterange(self, start_date, end_date):
-		# generator function, yielding a range of dates to be searched over.
-		start_date = datetime.datetime.strptime(start_date, '%d/%m/%Y')
-		end_date = datetime.datetime.strptime(end_date, '%d/%m/%Y')
-		for day in range(int((end_date - start_date).days + 1)):
-			yield start_date + datetime.timedelta(day)
-	
-	def exact_search(self, entry):
-		matches = {}
-		with open(self.FILE, 'r') as csvfile:
-			csv_reader = csv.reader(csvfile, delimiter=' ',
-									lineterminator='\n')
-			for line_num, line in enumerate(csv_reader):
-				if line == entry:
-					matches[line_num] = line
-	
-		return matches
-	
-	def regex_search(self, pattern):
-		# regex patterns are compared with the strings for entry titles and
-		# notes
-		matches = {}
-		with open(self.FILE, 'r') as csvfile:
-			csv_reader = csv.reader(csvfile, delimiter=' ',
-									lineterminator='\n')
-			for line_num, line in enumerate(csv_reader):
-				if re.search(pattern, line[1]) or re.search(pattern, line[3]):
-					matches[line_num] = line
-		return matches
-	
-	def edit_file(self, action, n, entry):
-		lines = {}
-		# the lines of the file are read into a dictionary ...
-		with open(self.FILE, 'r') as csvfile:
-			csv_reader = csv.reader(csvfile, delimiter=' ',
-									lineterminator='\n')
-	
-			for line_num, line in enumerate(csv_reader):
-				if line_num == n:
-					if action == 'edit':  # ... a line is amended ...
-						lines[n] = entry
-					if action == 'delete':  # ... or, a line is not included,..
-						continue
-				else:
-					lines[line_num] = line
-	
-		# ... the file is then rewritten
-		with open(self.FILE, 'w') as csvfile:
-			csv_writer = csv.writer(csvfile, delimiter=' ',
-									lineterminator='\n')
-			# ... using values of the new dictionary.
-			for num, line in lines.items():
-				csv_writer.writerow(lines[num])
+
+    '''Entry class is responsible for handling individual work log entries
+    stored in the accompanying .csv file for this project, work_log.csv
+    Entry class has methods for adding an entry to this file, searching for
+    entries within the file and editing entries within the
+    file, including deletion.
+    All matches to a search are stored in dictionaries, the key of each
+    dictionary indexing a search match; the value details the work log entry
+    details of the corresponding search match. Indexing entries within the
+    file allows entries to be edited, deleted as required.
+    '''
+
+    FILE = 'work_log.csv'
+
+    def add_entry_to_file(self, *args):
+        # entries are added to .csv file line by line
+
+        with open(self.FILE, 'a') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            csv_writer.writerow(args)
+
+    def search_by_date(self, date):
+        matches = {}
+        with open(self.FILE, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            for line_num, line in enumerate(csv_reader):
+                if line:
+                    if line[0] == date:
+                        matches[line_num] = line
+
+        return matches
+
+    def search_by_range_of_dates(self, start_date, end_date):
+        matched = {}
+        for date in self.daterange(start_date, end_date):
+            matched.update(self.search_by_date(date.strftime('%d/%m/%Y')))
+        return matched
+
+    def daterange(self, start_date, end_date):
+        # generator function, yielding a range of dates to be searched over.
+        start_date = datetime.datetime.strptime(start_date, '%d/%m/%Y')
+        end_date = datetime.datetime.strptime(end_date, '%d/%m/%Y')
+        for day in range(int((end_date - start_date).days + 1)):
+            yield start_date + datetime.timedelta(day)
+
+    def exact_search(self, entry):
+        matches = {}
+        with open(self.FILE, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            for line_num, line in enumerate(csv_reader):
+                if line == entry:
+                    matches[line_num] = line
+
+        return matches
+
+    def regex_search(self, pattern):
+        # regex patterns are compared with the strings for entry titles and
+        # notes
+        matches = {}
+        with open(self.FILE, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            for line_num, line in enumerate(csv_reader):
+                if re.search(pattern, line[1]) or re.search(pattern, line[3]):
+                    matches[line_num] = line
+        return matches
+
+    def search_by_time_spent(self, time):
+        matches = {}
+        with open(self.FILE, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            for line_num, line in enumerate(csv_reader):
+                if line:
+                    if line[2] == time:
+                        matches[line_num] = line
+
+        return matches
+
+    def edit_file(self, action, n, entry):
+        lines = {}
+        # the lines of the file are read into a dictionary ...
+        with open(self.FILE, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+
+            for line_num, line in enumerate(csv_reader):
+                if line_num == n:
+                    if action == 'edit':  # ... a line is amended ...
+                        lines[n] = entry
+                    if action == 'delete':  # ... or, a line is not included,..
+                        continue
+                else:
+                    lines[line_num] = line
+
+        # ... the file is then rewritten
+        with open(self.FILE, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=' ',
+                                    lineterminator='\n')
+            # ... using values of the new dictionary.
+            for num, line in lines.items():
+                csv_writer.writerow(lines[num])
 
--- work_log.py
+++ work_log.py
@@ -2,231 +2,248 @@ import datetime
 import os
 import sys
 import re
-	
+
 from entry import Entry
-	
+
 e = Entry()
-	
-	
+
+
 class Work_log():
-	
-	'''Worklog class provides functionality for user interactivity with
-	their work log entries.
-	The class provides a series of menu for adding entries and searching for
-	entries. Entries matching to a search can be viewed, edited and deleted.
-	Searches can be conducted according to the exact date of a work log
-	entry, a range of dates, the exact details of an entry, or by a user
-	supplied regular expression pattern.
-	Users are suitably prompted to make choices from menu, supply entry details
-	as required. Each menu and input prompt are linked for logical navigation.
-	There is also an option to end the program.
-	'''
 
-	def display_main_menu(self):
+    '''Worklog class provides functionality for user interactivity with
+    their work log entries.
+    The class provides a series of menu for adding entries and searching for
+    entries. Entries matching to a search can be viewed, edited and deleted.
+    Searches can be conducted according to the exact date of a work log
+    entry, a range of dates, the exact details of an entry, or by a user
+    supplied regular expression pattern.
+    Users are suitably prompted to make choices from menu, supply entry details
+    as required. Each menu and input prompt are linked for logical navigation.
+    There is also an option to end the program.
+    '''
 
-		self.clear_screen()
-		main_menu_options = {'a': self.add_entry, 'b': self.search_entries,
-							 'c': self.quit_program}
-		print('WORK LOG')
-		print('What would you like to do?')
-		print('a) Add new entry')
-		print('b) Search for existing entries')
-		print('c) Quit program')
+    def display_main_menu(self):
 
-		try:
-			main_menu_choice = main_menu_options[input('> ')]
-		except KeyError:
-			input('That is not a valid choice. Please enter to try again')
-			self.display_main_menu()
-		else:
-			main_menu_choice()
+        self.clear_screen()
+        main_menu_options = {'a': self.add_entry, 'b': self.search_entries,
+                             'c': self.quit_program}
+        print('WORK LOG')
+        print('What would you like to do?')
+        print('a) Add new entry')
+        print('b) Search for existing entries')
+        print('c) Quit program')
 
-	def add_entry(self):
+        try:
+            main_menu_choice = main_menu_options[input('> ')]
+        except KeyError:
+            input('That is not a valid choice. Please enter to try again')
+            self.display_main_menu()
+        else:
+            main_menu_choice()
 
-		e.add_entry_to_file(*self.get_entry_details())
-		input('The entry has been added. Please enter to return to the menu.')
-		self.display_main_menu()
+    def add_entry(self):
 
-	def get_entry_details(self):
+        e.add_entry_to_file(*self.get_entry_details())
+        input('The entry has been added. Please enter to return to the menu.')
+        self.display_main_menu()
 
-		date = self.get_date()
-		title = self.get_title()
-		time_spent = self.get_time_spent()
-		notes = self.get_notes()
-		return date, title, time_spent, notes
+    def get_entry_details(self):
 
-	def get_date(self):
+        date = self.get_date()
+        title = self.get_title()
+        time_spent = self.get_time_spent()
+        notes = self.get_notes()
+        return date, title, time_spent, notes
 
-		self.clear_screen()
-		print('Date of task:')
-		try:
-			date = datetime.datetime.strptime(input('> '), '%d/%m/%Y')
-		except ValueError:
-			input('That is not a valid time. Press enter to try again.')
-			self.get_date()
-		else:
-			return date.strftime('%d/%m/%Y')
+    def get_date(self):
 
-	def get_title(self):
+        self.clear_screen()
+        print('Date of task (DD/MM/YYYY):')
+        try:
+            date = datetime.datetime.strptime(input('> '), '%d/%m/%Y')
+        except ValueError:
+            input('That is not a valid time. Press enter to try again.')
+            self.get_date()
+        else:
+            return date.strftime('%d/%m/%Y')
 
-		self.clear_screen()
-		print('Title of task:')
+    def get_title(self):
 
-		title = input('> ')
-		if title:
-			return title
-		else:
-			input('You have not entered a title for the task. Press enter to '
-				  'try again')
-			self.get_title()
+        self.clear_screen()
+        print('Title of task:')
 
-	def get_time_spent(self):
+        title = input('> ')
+        if title:
+            return title
+        else:
+            input('You have not entered a title for the task. Press enter to '
+                  'try again')
+            self.get_title()
 
-		self.clear_screen()
-		print('Time spent (rounded to nearest minute)')
+    def get_time_spent(self):
 
-		try:
-			time_spent = int(input('> '))
-		except ValueError:
-			input('That is not a valid input. Press enter to try again.')
-			self.get_time_spent()
-		else:
-			return str(time_spent)
+        self.clear_screen()
+        print('Time spent (rounded to nearest minute)')
 
-	def get_notes(self):
+        try:
+            time_spent = int(input('> '))
+        except ValueError:
+            input('That is not a valid input. Press enter to try again.')
+            self.get_time_spent()
+        else:
+            return str(time_spent)
 
-		self.clear_screen()
-		print('Notes (Optional, leave blank if required)')
-		return input('>')
+    def get_notes(self):
 
-	def search_entries(self):
+        self.clear_screen()
+        print('Notes (Optional, leave blank if required)')
+        return input('>')
 
-		self.clear_screen()
-		search_menu_options = {'a': self.exact_date,
-							   'b': self.range_of_dates,
-							   'c': self.exact_search,
-							   'd': self.regex_search,
-							   'e': self.display_main_menu}
-		print('Do you want to search by?')
-		print('a) Exact date')
-		print('b) Range of dates')
-		print('c) Exact search')
-		print('d) Regex search')
-		print('e) Return to menu')
-		try:
-			search_menu_choice = search_menu_options[input('> ')]
-		except KeyError:
-			input('That is not a valid choice. Press enter to try again.')
-			self.search_entries()
-		else:
-			self.clear_screen()
-			search_menu_choice()
+    def search_entries(self):
 
-	def exact_date(self):
+        self.clear_screen()
+        search_menu_options = {'a': self.exact_date,
+                               'b': self.range_of_dates,
+                               'c': self.exact_search,
+                               'd': self.regex_search,
+                               'e': self.time_spent,
+                               'f': self.display_main_menu}
+        print('Do you want to search by?')
+        print('a) Exact date')
+        print('b) Range of dates')
+        print('c) Exact search')
+        print('d) Regex search')
+        print('e) Time spent')
+        print('f) Return to menu')
+        try:
+            search_menu_choice = search_menu_options[input('> ')]
+        except KeyError:
+            input('That is not a valid choice. Press enter to try again.')
+            self.search_entries()
+        else:
+            self.clear_screen()
+            search_menu_choice()
 
-		input('Search by exact date. Press any key to continue.')
-		self.search_results(e.search_by_date(self.get_date()))
+    def exact_date(self):
 
-	def range_of_dates(self):
+        input('Search by exact date. Press any key to continue.')
+        self.search_results(e.search_by_date(self.get_date()))
 
-		input('Search by a range of dates.\nPress enter to '
-			  'continue and enter start date.')
-		start_date = self.get_date()
-		self.clear_screen()
-		input('Press enter to continue and enter end date.')
-		end_date = self.get_date()
-		
-		if end_date < start_date:
-			input('End date has to be after start date. Press enter to try again.')
-			self.range_of_dates()
+    def range_of_dates(self):
 
-		self.search_results(e.search_by_range_of_dates(start_date, end_date))
+        input('Search by a range of dates.\nStart date.'
+              'Press any key to continue')
+        start_date = self.get_date()
+        input('End date. Press any key to continue')
+        end_date = self.get_date()
 
-	def exact_search(self):
+        self.search_results(e.search_by_range_of_dates(start_date, end_date))
 
-		input('Search by exact entry details. Press any key to continue')
-		entry = list(self.get_entry_details())
-		self.search_results(e.exact_search(entry))
+    def exact_search(self):
 
-	def regex_search(self):
+        input('Search by exact entry details. Press any key to continue')
+        entry = list(self.get_entry_details())
+        self.search_results(e.exact_search(entry))
 
-		try:
-			pattern = re.compile(
-				input("Enter a regex pattern (pattern will be applied to "
-					  "entry title and notes): "))
-		except re.error:
-			input('Not a valid pattern. Press enter to try again.')
-			self.clear_screen()
-			self.regex_search()
-		else:
-			self.search_results(e.regex_search(pattern))
+    def regex_search(self):
 
-	def search_results(self, matches):
+        try:
+            pattern = re.compile(
+                input("Enter a regex pattern (pattern will be applied to "
+                      "entry title and notes): "))
+        except re.error:
+            input('Not a valid pattern. Press enter to try again.')
+            self.clear_screen()
+            self.regex_search()
+        else:
+            self.search_results(e.regex_search(pattern))
 
-		count = 0
-		for line_num, match in matches.items():
-			count += 1
-			self.clear_screen()
-			print('Date : {}'.format(match[0]))
-			print('Title : {}'.format(match[1]))
-			print('Time Spent : {}'.format(match[2]))
-			print('Notes : {}'.format(match[3]))
-			print('Result {} of {}'.format(count, len(matches)))
+    def time_spent(self):
 
-			if count == len(matches):
-				print('[E]dit, [D]elete, [enter] to return to search menu')
-				search_result_options = 'ed'
-			else:
-				print('[N]ext, [E]dit, [D]elete, '
-					  '[enter] to return to search menu')
-				search_result_options = 'ned'
+        input('Search by time spent date. Press any key to continue.')
+        self.search_results(e.search_by_time_spent(self.get_time_spent()))
 
-			while True:
-				choice = input('> ')
-				if choice not in search_result_options:
-					input('Not a valid option. Press enter to try again.')
-				else:
-					break
-			if choice == 'n':
-				continue
-			elif choice == 'e':
-				self.edit_entry(line_num)
-			elif choice == 'd':
-				self.delete_entry(line_num, match)
-			else:
-				self.search_entries()
+    def search_results(self, matches):
 
-	def edit_entry(self, line_num):
+        if not matches:
+            print('There were no matches')
+            input('Press enter to return to the search menu.')
+            self.search_entries()
 
-		input('Edit entry. Press any key to enter edit')
-		entry = list(self.get_entry_details())
-		e.edit_file('edit', line_num, entry)
-		input('Edit made. Press enter to return to search menu')
-		self.search_entries()
+        index = []
+        for i in matches.keys():
+            index.append(i)
 
-	def delete_entry(self, line_num, match):
+        count = 0
+        while True:
+            self.clear_screen()
+            print('Date : {}'.format(matches[index[count]][0]))
+            print('Title : {}'.format(matches[index[count]][1]))
+            print('Time Spent : {}'.format(matches[index[count]][2]))
+            print('Notes : {}'.format(matches[index[count]][3]))
+            print('Result {} of {}'.format(count+1, len(index)))
+            if count == 0:
+                print('[N]ext, [E]dit, [D]elete, '
+                      '[enter] to return to search menu')
+                search_result_options = 'ned'
+            if count >= 1 and count < len(index)-1:
+                print('[N]ext, [P]revious [E]dit, [D]elete, '
+                      '[enter] to return to search menu')
+                search_result_options = 'nped'
 
-		input('Press any key to delete entry.')
-		e.edit_file('delete', line_num, match)
-		input('Entry deleted. Press enter to return to search menu')
-		self.search_entries()
+            if count == len(index)-1:
+                print('[P]revious [E]dit, [D]elete, [enter] to return to '
+                      'search '
+                      'menu')
+                search_result_options = 'ped'
 
-	def quit_program(self):
+            while True:
+                choice = input('> ')
+                if choice not in search_result_options:
+                    input('Not a valid option. Press enter to try again.')
+                else:
+                    break
+            if choice == 'n':
+                count += 1
+                continue
+            elif choice == 'p':
+                count -= 1
+                continue
+            elif choice == 'e':
+                self.edit_entry(index[count])
+            elif choice == 'd':
+                self.delete_entry(index[count], matches[index[count]])
+            break
+        self.search_entries()
 
-		self.clear_screen()
-		print('Thank you and goodbye.')
-		sys.exit()
+    def edit_entry(self, line_num):
 
-	def clear_screen(self):
+        input('Edit entry. Press any key to enter edit')
+        entry = list(self.get_entry_details())
+        e.edit_file('edit', line_num, entry)
+        input('Edit made. Press enter to return to search menu')
 
-		os.system('cls' if os.name == 'nt' else 'clear')
+    def delete_entry(self, line_num, match):
+
+        input('Press any key to delete entry.')
+        e.edit_file('delete', line_num, match)
+        input('Entry deleted. Press enter to return to search menu')
+
+    def quit_program(self):
+
+        self.clear_screen()
+        print('Thank you and goodbye.')
+        sys.exit()
+
+    def clear_screen(self):
+
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
-	w = Work_log()
-	w.display_main_menu()
-
+    w = Work_log()
+    w.display_main_menu()
 
 if __name__ == '__main__':
-	main()
+    main()
 
